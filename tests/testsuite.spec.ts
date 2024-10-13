7ef778f8d2f2adf0d3517c33b8940f155b9afa2a
Kriksy <viktoria.toldi@gmail.com>
2024-10-13 16:22:48 +0200

    Add testsuite with login

---
+++ tests/testsuite.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect, request, APIRequestContext } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+import { APIHelper } from "./apiHelpers";
+
+// Request context is reused by all tests in the file.
+let apiContext: APIRequestContext;
+
+test.beforeAll("Login", async ({ playwright, request }) => {
+  const loginResponse = await request.post("/api/login", {
+    data: {
+      username: process.env.TEST_USERNAME,
+      password: process.env.TEST_PASSWORD,
+    },
+  });
+
+  expect(loginResponse.ok()).toBeTruthy();
+  expect(loginResponse.status()).toBe(200);
+
+  const { token, username } = await loginResponse.json();
+
+  apiContext = await playwright.request.newContext({
+    baseURL: `${process.env.BASE_URL}`,
+    extraHTTPHeaders: {
+      // Add authorization token to all requests.
+      "x-user-auth": JSON.stringify({ username, token }),
+    },
+  });
+});
+
+test.afterAll(async ({}) => {
+  // Dispose all responses.
+  await apiContext.dispose();
+});
+
+test.describe("Test suite backend v1", () => {
+  let apiHelper: APIHelper;
+
+  test.beforeAll("Setup API Helper", () => {
+    apiHelper = new APIHelper("/");
+  });
+
+  test("Test case: Login", async ({}) => {});
+});
