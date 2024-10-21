eecf582f8ce08ce79458e23688296e0d8455050e
Kriksy <viktoria.toldi@gmail.com>
2024-10-21 23:17:04 +0200

    Add Client API helpers

--- tests/apiHelpers.ts
+++ tests/apiHelpers.ts
@@ -8,9 +8,45 @@ export class APIHelper {
     this.baseUrl = baseUrl;
     this.request = request;
   }
+  // ### CLIENT ###
 
+  // Create a new client
   async createClient(payload: object) {
     const response = await this.request.post(`/api/client/new`, {
+      data: JSON.stringify(payload),
+    });
+    return response;
+  }
+
+  // Get client by id
+  async getClientById(id: string) {
+    const response = await this.request.get(`/api/client/${id}`);
+    return response;
+  }
+
+  // Retrieve all clients
+  async getAllClients() {
+    const response = await this.request.get(`/api/clients`);
+    return response;
+  }
+
+  // Delete a client by id
+  async deleteClientById(id: string) {
+    const response = await this.request.delete(`/api/client/${id}`);
+    return response;
+  }
+
+  // Update a client by id
+  async updateClientById(id: string) {
+    const response = await this.request.put(`/api/client/${id}`);
+    return response;
+  }
+
+  // ### BILL ###
+
+  // Create a new client
+  async createBill(payload: object) {
+    const response = await this.request.post(`/api/bill/new`, {
       data: JSON.stringify(payload),
     });
     return response;
